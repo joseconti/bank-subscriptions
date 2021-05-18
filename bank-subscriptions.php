0619fcb2a5f07f08b1ea70e8dc7ff38d6357d55b
José Conti <j.conti@joseconti.com>
2021-05-18 17:40:51 +0200

    Añadido procesando

--- bank-subscriptions.php
+++ bank-subscriptions.php
@@ -84,6 +84,11 @@ function woocommerce_gateway_bank_transfer_subscriptions_init() {
 	            'name'   => __( 'Complete', 'woocommerce' ),
 	            'action' => 'complete',
 	        );
+	        $actions['processing'] = array(
+	            'url'    => wp_nonce_url( admin_url( 'admin-ajax.php?action=woocommerce_mark_order_status&status=processing&order_id=' . $order->get_id() ), 'woocommerce-mark-order-status' ),
+	            'name'   => __( 'Processing', 'woocommerce' ),
+	            'action' => 'processing',
+	        );
 	    }
 	    return $actions;
 	}
